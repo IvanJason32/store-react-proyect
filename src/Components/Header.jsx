8ff824218977cf20b0b42c9036926588feaad02c
IvanJason134 <jestrada1@ucol.mx>
2024-06-03 18:41:43 -0600

    add compound components to Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, createContext, useContext } from "react";
 import { useAuth } from "../hooks/useAuth";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
@@ -6,12 +6,14 @@ import { useFetchCollection } from "../hooks/useFetchCollection";
 import { useSelector } from "react-redux";
 import carritoImg from "../assets/carritoCompra.png";
 
-const Header = () => {
+export const NavbarContext = createContext();
+
+const Header = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { token, logout } = useAuth();
   const { data, isPending, getData } = useFetchCollection();
   const [uniqueTitles, setUniqueTitles] = useState([]);
   const { totalItems } = useSelector((store) => store.cart);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     getData();
@@ -29,208 +31,162 @@ const Header = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-content">
-      <div className="navbar-toggle" onClick={toggleMenu}>
-        ☰
-      </div>
-      <div className={`navbar-opcions-deskopt ${isMenuOpen ? "open" : ""}`}>
-        <NavLink
-          to="/home"
-          className={`title-opc ${({ isActive }) =>
-            isActive ? "active" : ""}`}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/categorias"
-          className={`title-opc ${({ isActive }) =>
-            isActive ? "active" : ""}`}
-        >
-          Categories
-        </NavLink>
-        {uniqueTitles
-          ? uniqueTitles.map((element, index) => {
-              return (
-                <NavLink
-                  key={index}
-                  to={`/categorias/${element}`}
-                  className={`title-opc ${({ isActive }) =>
-                    isActive ? "active" : ""}`}
-                >
-                  {element}
-                </NavLink>
-              );
-            })
-          : ""}
-      </div>
-      <div className="loginRegister">
-        <NavLink
-          to="/carrito"
-          className={`title-opc ${({ isActive }) =>
-            isActive ? "active" : ""}`}
-        >
-          <div className="cont-cart">
-            <img className="img-carrito" src={carritoImg} alt="" />
-            {totalItems !== 0 ? <div className="circul">{totalItems}</div> : ""}
-          </div>
-        </NavLink>
-        {token ? (
-          <NavLink
-            onClick={logout}
-            style={{
-              color: "red",
-            }}
-            to="/login"
-            className={`title-opc ${({ isActive }) =>
-              isActive ? "active" : ""}`}
-          >
-            Logout
-          </NavLink>
-        ) : (
-          <NavLink
-            to="/login"
-            className={`title-opc ${({ isActive }) =>
-              isActive ? "active" : ""}`}
-          >
-            Login
-          </NavLink>
-        )}
-      </div>
-      </div>
-      <div className={`navbar-opcions-mobile ${isMenuOpen ? "open" : ""}`}>
-        <NavLink
-        onClick={() => setIsMenuOpen(false)}
-          to="/home"
-          className={`title-opc ${({ isActive }) =>
-            isActive ? "active" : ""}`}
-        >
-          Home
-        </NavLink>
-        <NavLink
-        onClick={() => setIsMenuOpen(false)}
-          to="/categorias"
-          className={`title-opc ${({ isActive }) =>
-            isActive ? "active" : ""}`}
-        >
-          Categories
-        </NavLink>
-        {uniqueTitles
-          ? uniqueTitles.map((element, index) => {
-              return (
-                <NavLink
-                onClick={() => setIsMenuOpen(false)}
-                  key={index}
-                  to={`/categorias/${element}`}
-                  className={`title-opc ${({ isActive }) =>
-                    isActive ? "active" : ""}`}
-                >
-                  {element}
-                </NavLink>
-              );
-            })
-          : ""}
-      </div>
-      
-    </nav>
+    <NavbarContext.Provider
+      value={{
+        isMenuOpen,
+        toggleMenu,
+        token,
+        logout,
+        uniqueTitles,
+        totalItems,
+        setIsMenuOpen,
+      }}
+    >
+      <nav className="navbar">{children}</nav>
+    </NavbarContext.Provider>
   );
 };
 
+const Content = ({ children }) => {
+  return <div className="navbar-content">{children}</div>;
+};
+
+const Toggle = () => {
+  const { toggleMenu } = useContext(NavbarContext);
+  return (
+    <div className="navbar-toggle" onClick={toggleMenu}>
+      ☰
+    </div>
+  );
+};
+
+const DesktopOptions = ({ children }) => {
+  return <div className="navbar-opcions-deskopt">{children}</div>;
+};
+
+const MobileOptions = ({ children }) => {
+  const { isMenuOpen } = useContext(NavbarContext);
+  return (
+    <div className={`navbar-opcions-mobile ${isMenuOpen ? "open" : ""}`}>
+      {children}
+    </div>
+  );
+};
+
+const NavBarOpctions = ({ to, children, onClick }) => {
+  return (
+    <NavLink
+      to={to}
+      onClick={onClick}
+      className={`title-opc ${({ isActive }) => (isActive ? "active" : "")}`}
+    >
+      {children}
+    </NavLink>
+  );
+};
+
+const NavBarOpctionsMobile = ({ to, children, onClick }) => {
+  const { setIsMenuOpen } = useContext(NavbarContext);
+  return (
+    <NavLink
+      to={to}
+      onClick={() => setIsMenuOpen(false)}
+      className={`title-opc ${({ isActive }) => (isActive ? "active" : "")}`}
+    >
+      {children}
+    </NavLink>
+  );
+};
+
+const NavBarOpcCategorys = () => {
+  const { uniqueTitles } = useContext(NavbarContext);
+  return uniqueTitles
+    ? uniqueTitles.map((element, index) => {
+        return (
+          <NavLink
+            key={index}
+            to={`/categorias/${element}`}
+            className={`title-opc ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+          >
+            {element}
+          </NavLink>
+        );
+      })
+    : "";
+};
+
+const NavBarOpcCategorysMobile = () => {
+  const { uniqueTitles, setIsMenuOpen } = useContext(NavbarContext);
+  return uniqueTitles
+    ? uniqueTitles.map((element, index) => {
+        return (
+          <NavLink
+            onClick={() => setIsMenuOpen(false)}
+            key={index}
+            to={`/categorias/${element}`}
+            className={`title-opc ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+          >
+            {element}
+          </NavLink>
+        );
+      })
+    : "";
+};
+
+const Cart = () => {
+  const { totalItems } = useContext(NavbarContext);
+  return (
+    <NavLink
+      to="/carrito"
+      className={`title-opc ${({ isActive }) => (isActive ? "active" : "")}`}
+    >
+      <div className="cont-cart">
+        <img className="img-carrito" src={carritoImg} alt="" />
+        {totalItems !== 0 ? <div className="circul">{totalItems}</div> : ""}
+      </div>
+    </NavLink>
+  );
+};
+
+const AuthLink = () => {
+  const { token, logout } = useContext(NavbarContext);
+  return token ? (
+    <NavLink
+      onClick={logout}
+      style={{
+        color: "red",
+      }}
+      to="/login"
+      className={`title-opc ${({ isActive }) => (isActive ? "active" : "")}`}
+    >
+      Logout
+    </NavLink>
+  ) : (
+    <NavLink
+      to="/login"
+      className={`title-opc ${({ isActive }) => (isActive ? "active" : "")}`}
+    >
+      Login
+    </NavLink>
+  );
+};
+
+const NavLoginSeccion = ({ children }) => {
+  return <div className="loginRegister">{children}</div>;
+};
+
+Header.Content = Content;
+Header.Toggle = Toggle;
+Header.DesktopOptions = DesktopOptions;
+Header.MobileOptions = MobileOptions;
+Header.NavBarOpctions = NavBarOpctions;
+Header.NavBarOpcCategorys = NavBarOpcCategorys;
+Header.Cart = Cart;
+Header.AuthLink = AuthLink;
+Header.NavLoginSeccion = NavLoginSeccion;
+Header.NavBarOpcCategorysMobile = NavBarOpcCategorysMobile;
+Header.NavBarOpctionsMobile = NavBarOpctionsMobile;
+
 export default Header;
-
-// import { useEffect, useState } from "react";
-// import { useAuth } from "../hooks/useAuth";
-// import "./Header.css";
-// import { NavLink } from "react-router-dom";
-// import { useFetchCollection } from "../hooks/useFetchCollection";
-// import { useSelector } from "react-redux";
-// import carritoImg from "../assets/carritoCompra.png";
-
-// const Header = () => {
-//   const { token, logout } = useAuth();
-//   const { data, isPending, getData } = useFetchCollection();
-//   const [uniqueTitles, setUniqueTitles] = useState([]);
-//   const { totalItems } = useSelector((store) => store.cart);
-//   console.log(totalItems);
-
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   useEffect(() => {
-//     if (!isPending) {
-//       const titles = [...new Set(data.map((item) => item.title))];
-//       setUniqueTitles(titles);
-//     }
-//   }, [data, isPending]);
-
-//   return (
-//     <nav className="navbar">
-//       <div className="navbar-opcions">
-//         <NavLink
-//           to="/home"
-//           className={`title-opc ${({ isActive }) =>
-//             isActive ? "active" : ""}`}
-//         >
-//           Home
-//         </NavLink>
-//         <NavLink
-//           to="/categorias"
-//           className={`title-opc ${({ isActive }) =>
-//             isActive ? "active" : ""}`}
-//         >
-//           Categories
-//         </NavLink>
-//         {uniqueTitles
-//           ? uniqueTitles.map((element, index) => {
-//               return (
-//                 <NavLink
-//                   key={index}
-//                   to={`/categorias/${element}`}
-//                   className={`title-opc ${({ isActive }) =>
-//                     isActive ? "active" : ""}`}
-//                 >
-//                   {element}
-//                 </NavLink>
-//               );
-//             })
-//           : ""}
-//       </div>
-//       <div className="loginRegister">
-//         <NavLink
-//           to="/carrito"
-//           className={`title-opc ${({ isActive }) =>
-//             isActive ? "active" : ""}`}
-//         >
-//           <div className="cont-cart">
-//             <img className="img-carrito" src={carritoImg} alt="" />
-//             {totalItems !== 0 ? <div className="circul">{totalItems}</div> : ""}
-//           </div>
-//         </NavLink>
-//         {token ? (
-//           <NavLink
-//             onClick={logout}
-//             style={{
-//               color: "red",
-//             }}
-//             to="/login"
-//             className={`title-opc ${({ isActive }) =>
-//               isActive ? "active" : ""}`}
-//           >
-//             Logout
-//           </NavLink>
-//         ) : (
-//           <NavLink
-//             to="/login"
-//             className={`title-opc ${({ isActive }) =>
-//               isActive ? "active" : ""}`}
-//           >
-//             Login
-//           </NavLink>
-//         )}
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Header;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,13 +2,40 @@ import Header from "../Components/Header";
 import { Outlet } from "react-router-dom";
 import "./Root.css";
 import { SelectedProductsProvider } from "../context/ProductContext";
+// import { useContext } from "react";
 
 const Root = () => {
+  // const { uniqueTitles, setIsMenuOpen } = useContext(NavbarContext);
+
   return (
     <div className="principal-structure">
       <SelectedProductsProvider>
         <div className="container-header">
-          <Header />
+          <Header>
+            <Header.Content>
+              <Header.Toggle />
+              <Header.DesktopOptions>
+                <Header.NavBarOpctions to="/home">Home</Header.NavBarOpctions>
+                <Header.NavBarOpctions to="/categorias">
+                  Categories
+                </Header.NavBarOpctions>
+                <Header.NavBarOpcCategorys />
+              </Header.DesktopOptions>
+              <Header.NavLoginSeccion>
+                <Header.Cart />
+                <Header.AuthLink />
+              </Header.NavLoginSeccion>
+            </Header.Content>
+            <Header.MobileOptions>
+              <Header.NavBarOpctionsMobile to="/home">
+                Home
+              </Header.NavBarOpctionsMobile>
+              <Header.NavBarOpctionsMobile to="/categorias">
+                Categories
+              </Header.NavBarOpctionsMobile>
+              <Header.NavBarOpcCategorysMobile />
+            </Header.MobileOptions>
+          </Header>
         </div>
         <div className="container-body">
           <Outlet />
